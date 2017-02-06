72d13838de06fba899bd95907f02a5116f838843
Khurram Nizami <ceo@medrender.com>
2017-02-06 15:17:29 -0600

    added uglify package

--- gruntfile.js
+++ gruntfile.js
@@ -19,6 +19,7 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
