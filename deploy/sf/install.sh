05f10b76a14500aec6f148686b344f7c7f7514c7
Khurram Nizami <ceo@medrender.com>
2017-02-10 17:00:20 -0600

    added version update for ServiceManifest

--- deploy/sf/install.sh
+++ deploy/sf/install.sh
@@ -4,22 +4,26 @@ echo "Deploying release to dev for Build ID: $BUILD_BUILDNUMBER"
 echo "================================================="
 echo "Step 1:  Update the manifest versions to: $BUILD_BUILDNUMBER"
 echo "================================================="
-echo "Updating ApplicationManifest.."
+echo "---Updating ApplicationManifest.."
 sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update Application Manifest, Exiting..."; exit 1; }
-echo "Updating ApplicationManifest Completed"
-echo "Updating ServiceManifestRef.."
+echo "---Updating ApplicationManifest Completed"
+echo "---Updating ServiceManifestRef.."
 sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ServiceManifestRef, Exiting..."; exit 1; }
 echo "Updating ServiceManifestRef Completed"
-echo "Updating CodePackage.."
+echo "---Updating ServiceManifest.."
+sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
+[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
+echo "---Updating ServiceManifest Completed"
+echo "---Updating CodePackage.."
 sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-echo "Updating CodePackage Completed"
-echo "Updating ImageName.."
+echo "---Updating CodePackage Completed"
+echo "---Updating ImageName.."
 sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\ImageName>/g" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
-echo "Updating ImageName Completed"
+echo "---Updating ImageName Completed"
 echo "================================================="
 echo "\n"
 echo "================================================="
