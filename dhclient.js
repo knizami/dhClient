5651860cf90c453b838803782c1ab263df50af9a
Khurram Nizami <ceo@medrender.com>
2017-02-13 23:54:34 -0600

    refactored dhclient

--- dhclient.js
+++ dhclient.js
@@ -90,207 +90,209 @@ console.log("DreamHome.dhClient service ==> Begin Execution.");
 
 // wait for DB module to fully initialize and connect to the backend DB
 // we don't want to start the node.js server listening till we know we are fully connected to the DB
-helper.dbInit(function (err) {
-  if (!err) { // DB connections have been established. 
-    console.log('  ... application has successfully connected to the DB');
-  } else { // OH no! something has gone wrong building DB connections!
-    // we still proceed and start the server listening
-    // but we mark the server as having a severe DB connection error!
-    console.log('  ... WARNING: application failed to connect with the backend DB!');
-  }
+// fetch/get a client/customer record by clientId (cid)
+//-----------------------------------------------------------------------------
+app.get('/client/:cid', function (req, res) {
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
 
-  //-----------------------------------------------------------------------------
-  // fetch/get a client/customer record by clientId (cid)
-  //-----------------------------------------------------------------------------
-  app.get('/client/:cid', function (req, res) {
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
+  // get the clientId (cid) parm that came in on the request
+  // it comes in as a string and MUST be converted to a base10 integer
+  var cid = parseInt(req.params.cid, 10);
 
-    // get the clientId (cid) parm that came in on the request
-    // it comes in as a string and MUST be converted to a base10 integer
-    var cid = parseInt(req.params.cid, 10);
+  var cref = helper.crefClient(); // obtain the dhClient collection handle/refrence
+  var dbQuery = {
+    'clientId': cid
+  }; // setup the query for locating the client record by cid
 
-    var cref = helper.crefClient(); // obtain the dhClient collection handle/refrence
-    var dbQuery = {
-      'clientId': cid
-    }; // setup the query for locating the client record by cid
+  //console.log('  ... dbQuery ('+JSON.stringify(dbQuery)+')');
 
-    //console.log('  ... dbQuery ('+JSON.stringify(dbQuery)+')');
+  // fetch the record from the collection based on the query desired.
+  cref.findOne(dbQuery, function (err, dbData) {
+    // test for error and be sure we found the data record
+    if (!err && dbData) {
+      //console.log('GET /client/:'+cid+' dbData('+JSON.stringify(dbData)+')');
+      // set the return json as the record found
+      retjson = dbData;
+    } else { // query failed
+      // log an error msg
+      console.error('GET /client/:' + cid + ' failed to get/read client record (' + cid + ') from DB!');
 
-    // fetch the record from the collection based on the query desired.
-    cref.findOne(dbQuery, function (err, dbData) {
-      // test for error and be sure we found the data record
-      if (!err && dbData) {
-        //console.log('GET /client/:'+cid+' dbData('+JSON.stringify(dbData)+')');
-        // set the return json as the record found
-        retjson = dbData;
-      } else { // query failed
-        // log an error msg
-        console.error('GET /client/:' + cid + ' failed to get/read client record (' + cid + ') from DB!');
+      retjson = {};
+      retjson.RC = _rcError;
+      retjson.error = "Client record(" + cid + ") not found!";
 
-        retjson = {};
-        retjson.RC = _rcError;
-        retjson.error = "Client record(" + cid + ") not found!";
-
-        // set http status code
-        statusCode = 404; // 404 not found
-      }
-
-      // send the http response message
-      helper.httpJsonResponse(res, statusCode, retjson);
-    });
-
-    return;
-  });
-
-  //-----------------------------------------------------------------------------
-  // functions to get/fetch records from the collections
-  //-----------------------------------------------------------------------------
-  app.get('/clients', function (req, res) {
-    //  console.log("app.get(./clients function has been called.");
-
-    var cref = helper.crefClient();
-    var dbQuery = {}; // query used for looking up records in the collection
-
-    // fetch records from the collection based on the query desired.
-    cref.find(dbQuery).toArray(function (err, items) {
-      if (!err) {
-        // send the http response message
-        var retjson = {
-          "RC": _rcOK
-        }; // assume a good json response
-        var statusCode = 200; // assume valid http response code=200 (OK, good response)
-
-        retjson.items = items;
-
-        // send the http response message
-        helper.httpJsonResponse(res, statusCode, retjson);
-      }
-    });
-
-    return;
-  });
-
-  //-----------------------------------------------------------------------------
-  // Delete a client customer record
-  //-----------------------------------------------------------------------------
-  app.delete('/client/:cid', function (req, res) {
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
-
-    // get the clientId (cid) parm that came in on the request
-    // it comes in as a string and MUST be converted to a base10 integer
-    var cid = parseInt(req.params.cid, 10);
-
-    var cref = helper.crefClient(); // obtain the dhClient collection handle/refrence
-    var dbQuery = {
-      'clientId': cid
-    }; // setup the query for locating the client record by cid
-
-    //console.log('  ... dbQuery ('+JSON.stringify(dbQuery)+')');
+      // set http status code
+      statusCode = 404; // 404 not found
+    }
 
     // send the http response message
-    retjson.success = "Delete client record(" + cid + ").";
-    res.status(statusCode).json(retjson);
-    res.end;
-
-    return;
+    helper.httpJsonResponse(res, statusCode, retjson);
   });
 
-  //-----------------------------------------------------------------------------
-  // Create a new client customer record
-  //-----------------------------------------------------------------------------
-  app.post('/client', function (req, res) {
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
-    var clientRecord = req.body; // get the request body json data
+  return;
+});
 
-    // add the client record to the DB
-    _addClientRecord(clientRecord, function (result) {
-      // send the http response message
-      retjson.data = result; // put the record added in the reply
-      retjson.success = "Created a new client record.";
-      res.status(statusCode).json(retjson);
-      res.end;
-    });
+//-----------------------------------------------------------------------------
+// functions to get/fetch records from the collections
+//-----------------------------------------------------------------------------
+app.get('/clients', function (req, res) {
+  //  console.log("app.get(./clients function has been called.");
+  helper.dbInit(function (err) {
+    if (!err) { // DB connections have been established. 
+      console.log('  ... application has successfully connected to the DB');
+      var cref = helper.crefClient();
+      var dbQuery = {}; // query used for looking up records in the collection
 
-    return;
+      // fetch records from the collection based on the query desired.
+      cref.find(dbQuery).toArray(function (err, items) {
+        if (!err) {
+          // send the http response message
+          var retjson = {
+            "RC": _rcOK
+          }; // assume a good json response
+          var statusCode = 200; // assume valid http response code=200 (OK, good response)
+
+          retjson.items = items;
+
+          // send the http response message
+          helper.httpJsonResponse(res, statusCode, retjson);
+        }
+      });
+
+    } else { // OH no! something has gone wrong building DB connections!
+      // we still proceed and start the server listening
+      // but we mark the server as having a severe DB connection error!
+      console.log('  ... WARNING: application failed to connect with the backend DB!');
+    }
+    //-----------------------------------------------------------------------------
   });
+});
 
-  //-----------------------------------------------------------------------------
-  // Update a client customer record
-  //-----------------------------------------------------------------------------
-  app.put('/client', function (req, res) {
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
-    var clientRecord = req.body; // get the request body json data
+//-----------------------------------------------------------------------------
+// Delete a client customer record
+//-----------------------------------------------------------------------------
+app.delete('/client/:cid', function (req, res) {
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
 
-    _updateClientRecord(clientRecord, function () {
-      // send the http response message
-      retjson.success = "Update a client record!";
-      res.status(statusCode).json(retjson);
-      res.end;
-    });
+  // get the clientId (cid) parm that came in on the request
+  // it comes in as a string and MUST be converted to a base10 integer
+  var cid = parseInt(req.params.cid, 10);
 
-    return;
-  });
+  var cref = helper.crefClient(); // obtain the dhClient collection handle/refrence
+  var dbQuery = {
+    'clientId': cid
+  }; // setup the query for locating the client record by cid
 
-  //-----------------------------------------------------------------------------
-  // Patch a client customer record
-  //-----------------------------------------------------------------------------
-  app.patch('/client', function (req, res) {
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
-    var clientRecord = req.body; // get the request body json data
+  //console.log('  ... dbQuery ('+JSON.stringify(dbQuery)+')');
 
-    _updateClientRecord(clientRecord, function () {
-      // send the http response message
-      retjson.success = "Patch a client record!";
-      res.status(statusCode).json(retjson);
-      res.end;
-    });
+  // send the http response message
+  retjson.success = "Delete client record(" + cid + ").";
+  res.status(statusCode).json(retjson);
+  res.end;
 
-    return;
-  });
+  return;
+});
 
-  //-----------------------------------------------------------------------------
-  // Simple echo get method, used to sanity test service
-  //-----------------------------------------------------------------------------
-  app.get('/echo', function (req, res) {
-    //  console.log("app.get(./echo function has been called.");
+//-----------------------------------------------------------------------------
+// Create a new client customer record
+//-----------------------------------------------------------------------------
+app.post('/client', function (req, res) {
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
+  var clientRecord = req.body; // get the request body json data
 
-    var retjson = {
-      "RC": _rcOK
-    }; // assume a good json response
-    var statusCode = 200; // assume valid http response code=200 (OK, good response)
-
+  // add the client record to the DB
+  _addClientRecord(clientRecord, function (result) {
     // send the http response message
-    retjson.success = "Echo from DreamHome.dhClient service!";
+    retjson.data = result; // put the record added in the reply
+    retjson.success = "Created a new client record.";
     res.status(statusCode).json(retjson);
     res.end;
-
-    return;
   });
 
+  return;
+});
 
-  // Start the node.js server listening
-  // even if the backend DB connection fails we still want to service requests
-  app.listen(_port);
+//-----------------------------------------------------------------------------
+// Update a client customer record
+//-----------------------------------------------------------------------------
+app.put('/client', function (req, res) {
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
+  var clientRecord = req.body; // get the request body json data
 
-  console.log('  ... application now listening on port ' + _port);
+  helper.dbInit(function (err) {
+    if (!err) { // DB connections have been established. 
+      console.log('  ... application has successfully connected to the DB');
+      _updateClientRecord(clientRecord, function () {
+        // send the http response message
+        retjson.success = "Update a client record!";
+        res.status(statusCode).json(retjson);
+        res.end;
+      });
+
+    } else { // OH no! something has gone wrong building DB connections!
+      // we still proceed and start the server listening
+      // but we mark the server as having a severe DB connection error!
+      console.log('  ... WARNING: application failed to connect with the backend DB!');
+    }
+
+    //-----------------------------------------------------------------------------
+
+  });
+});
+//-----------------------------------------------------------------------------
+// Patch a client customer record
+//-----------------------------------------------------------------------------
+app.patch('/client', function (req, res) {
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
+  var clientRecord = req.body; // get the request body json data
+
+  _updateClientRecord(clientRecord, function () {
+    // send the http response message
+    retjson.success = "Patch a client record!";
+    res.status(statusCode).json(retjson);
+    res.end;
+  });
+
+  return;
+});
+
+//-----------------------------------------------------------------------------
+// Simple echo get method, used to sanity test service
+//-----------------------------------------------------------------------------
+app.get('/echo', function (req, res) {
+  //  console.log("app.get(./echo function has been called.");
+
+  var retjson = {
+    "RC": _rcOK
+  }; // assume a good json response
+  var statusCode = 200; // assume valid http response code=200 (OK, good response)
+
+  // send the http response message
+  retjson.success = "Echo from DreamHome.dhClient service!";
+  res.status(statusCode).json(retjson);
+  res.end;
+
+  return;
 });
 
 
+// Start the node.js server listening
+// even if the backend DB connection fails we still want to service requests
 
 
 //-----------------------------------------------------------------------------
@@ -360,4 +362,5 @@ function _getReqBody(req, callback) {
   });
 }
 
+app.listen(_port);
 module.exports = app;
--- test/dhclient-test.js
+++ test/dhclient-test.js
@@ -1,20 +1,20 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../dhclient');
+var server = require('../dhclient');
 
 let should = chai.should();
 
 chai.use(chaiHttp);
 
-
 describe('/GET clients', () => {
+
     it('it should GET all the clients', (done) => {
         chai.request(server)
             .get('/clients')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('RC').and.equal(0);
-                res.body.should.have.property('items').and.should.be.a('array');
+                res.body.should.have.property('items').should.be.a('array');
                 //res.body.should.be.a('array');
                 res.body.length.should.not.be.eql(0);
                 done();
