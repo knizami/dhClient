974ccc42241948812afa611a3fa8fc2f1c0b4155
Khurram Nizami <knizami@gmail.com>
2017-02-14 08:52:14 -0600

    updated test conditions

--- test/dhclient-test.js
+++ test/dhclient-test.js
@@ -7,17 +7,14 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('/GET clients', () => {
-
     it('it should GET all the clients', (done) => {
         chai.request(server)
             .get('/clients')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('RC').and.equal(0);
-                res.body.should.have.property('items');
-                //res.body.items.should.be.a('array');
-                //res.body.should.be.a('object');
-                //res.body.length.should.not.be.eql(0);
+                res.body.should.have.property('items').that.is.an('array');
+                res.header.should.have.property('content-length').above(0);
                 done();
             });
     });
