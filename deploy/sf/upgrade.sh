fcb0259f0b6de11318cfb2bd71e83f737219505b
Khurram Nizami <ceo@medrender.com>
2017-02-15 14:37:15 -0600

    added upgrade script..

--- deploy/sf/upgrade.sh
+++ deploy/sf/upgrade.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
-[ -z $BUILDID ] && { echo '$BUILDID is undefined, exiting.'; exit 1; }
-echo "Deploying release to dev for Build ID: $BUILDID"
-echo "Step 1:  Update the manifest versions to: $BUILDID"
+[ -z $BUILD_BUILDNUMBER ] && { echo '$BUILDID is undefined, exiting.'; exit 1; }
+echo "Deploying release to dev for Build ID: $BUILD_BUILDNUMBER"
 echo "================================================="
-sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILDID\"/" ApplicationManifest.xml
+echo "Step 1:  Update the manifest versions to: $BUILD_BUILDNUMBER"
+echo "================================================="
+echo "---Updating ApplicationManifest.."
+sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update Application Manifest, Exiting..."; exit 1; }
-sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILDID\"/" ApplicationManifest.xml
+echo "---Updating ApplicationManifest Completed"
+echo "---Updating ServiceManifestRef.."
+sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ServiceManifestRef, Exiting..."; exit 1; }
-sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILDID\"/" dhClient/ServiceManifest.xml
+echo "Updating ServiceManifestRef Completed"
+echo "---Updating ServiceManifest.."
+sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILDID<\ImageName>/g" dhClient/ServiceManifest.xml
+echo "---Updating ServiceManifest Completed"
+echo "---Updating CodePackage.."
+sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
+[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
+echo "---Updating CodePackage Completed"
+echo "---Updating ImageName.."
+sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
-echo "===================COMPLETED====================="
-azure servicefabric application package copy dhClient fabric:ImageStore
-azure servicefabric application type register dhClient
-azure servicefabric application upgrade start -m Monitored fabric:/dh $BUILDID
+echo "---Updating ImageName Completed"
+echo "================================================="
+echo "\n"
+echo "================================================="
+echo "Step 2:  Connect to cluster: $SERVICEFABRICCLUSTER"
+echo "================================================="
+azure servicefabric cluster connect $SERVICEFABRICCLUSTER
+[ $? -eq 0 ] || { echo "Failed to connect to $SERVICEFABRICCLUSTER, Exiting..."; exit 1; }
+echo "================================================="
+echo "Step 3:  Copy to ImageStore"
+echo "================================================="
+azure servicefabric application package copy dhclient fabric:ImageStore
+[ $? -eq 0 ] || { echo "Failed to copy to ImageStore, Exiting..."; exit 1; }
+echo "================================================="
+echo "Step 4:  Register Application"
+echo "================================================="
+azure servicefabric application type register dhclient
+[ $? -eq 0 ] || { echo "Failed to register application, Exiting..."; exit 1; }
+echo "================================================="
+echo "Step 5:  Create Application"
+echo "================================================="
+azure servicefabric application upgrade start --application-name fabric:/dh --target-application-type-version $BUILD_BUILDNUMBER --rolling-upgrade-mode Monitored 
+[ $? -eq 0 ] || { echo "Failed to create application, Exiting..."; exit 1; }
