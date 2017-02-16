e8ff94734d7cdfe6b71770c35b1a08266b8da48c
Khurram Nizami <ceo@medrender.com>
2017-02-16 11:47:26 -0600

    fixed bug in slack script

--- deploy/slackpost.sh
+++ deploy/slackpost.sh
@@ -2,6 +2,6 @@
 curl -G -v "https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN" \
 --data-urlencode "channel=#vsts-dh-build" \
 --data-urlencode "text=$1" \
---data-urlencode "username="VSTS Build" \
+--data-urlencode "username=VSTS Build" \
 --data-urlencode "as_user=false" \
 --data-urlencode "pretty=1"
