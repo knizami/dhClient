3a33507f7b204c0ebed8446fa39f5b9561bbd977
Khurram Nizami <ceo@medrender.com>
2017-02-16 08:28:46 -0600

    reverted changes to absolute path

--- deploy/sf/upgrade.sh
+++ deploy/sf/upgrade.sh
@@ -5,23 +5,23 @@ echo "================================================="
 echo "Step 1:  Update the manifest versions to: $BUILD_BUILDNUMBER"
 echo "================================================="
 echo "---Updating ApplicationManifest.."
-sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" $(System.DefaultWorkingDirectory)/dep/deploy/sf/dhclient/ApplicationManifest.xml
+sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update Application Manifest, Exiting..."; exit 1; }
 echo "---Updating ApplicationManifest Completed"
 echo "---Updating ServiceManifestRef.."
-sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" $(System.DefaultWorkingDirectory)/dep/deploy/sf/dhclient/ApplicationManifest.xml
+sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ServiceManifestRef, Exiting..."; exit 1; }
 echo "Updating ServiceManifestRef Completed"
 echo "---Updating ServiceManifest.."
-sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" $(System.DefaultWorkingDirectory)/dep/deploy/sf/dhclient/dhClient/ServiceManifest.xml
+sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
 echo "---Updating ServiceManifest Completed"
 echo "---Updating CodePackage.."
-sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" $(System.DefaultWorkingDirectory)/dep/deploy/sf/dhclient/dhClient/ServiceManifest.xml
+sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
 echo "---Updating CodePackage Completed"
 echo "---Updating ImageName.."
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\/ImageName>/" $(System.DefaultWorkingDirectory)/dep/deploy/sf/dhclient/dhClient/ServiceManifest.xml
+sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
 echo "---Updating ImageName Completed"
 echo "================================================="
