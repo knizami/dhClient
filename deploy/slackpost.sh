f82205d941eaf7b62c6800a666b45e8df9c231db
Khurram Nizami <ceo@medrender.com>
2017-02-16 12:04:06 -0600

    updated to include token as parameter

--- deploy/slackpost.sh
+++ deploy/slackpost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -G -v "https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN" \
+curl -G -v "https://slack.com/api/chat.postMessage?token=$1" \
 --data-urlencode "channel=#vsts-dh-build" \
---data-urlencode "text=$1" \
+--data-urlencode "text=$2" \
 --data-urlencode "username=VSTS Build" \
 --data-urlencode "as_user=false" \
 --data-urlencode "pretty=1"
